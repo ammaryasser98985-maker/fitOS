b7c6895b3365e8906874ffff6d5f04af8bb97170
ammaryasser98985-maker <ammaryasser98985@gmail.com>
2026-04-30 20:32:27 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import FitOS from './FitOS.jsx'
+import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
